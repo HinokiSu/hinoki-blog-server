de5faf4ce933a85c08a0e68257fcda6bad65152f
Hinoki <yukihinoki@qq.com>
2022-01-06 15:37:52 +0800

    chore(admin): update inf of article

--- libs/db/src/interfaces/article.interface.ts
+++ libs/db/src/interfaces/article.interface.ts
@@ -3,8 +3,9 @@
 import { Document } from 'mongoose'
 
 export interface ArticleDocument extends Document {
-  readonly articleId: string
   readonly title: string
   readonly author: string
-  readonly date: string
+  readonly cate_id: string
+  readonly content: string
+  readonly content_md: string
 }
