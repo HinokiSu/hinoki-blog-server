2979d33e0f8ecf0a3a4332088d720ca3daf83851
Hinoki <yukihinoki@qq.com>
2022-02-16 22:48:12 +0800

    feat(server): add swagger&modify  url

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const config = new DocumentBuilder()
+    .setTitle('Blog Server(Client)')
+    .setDescription(`The Blog's Server API description`)
+    .setVersion('1.0')
+    // .addTag('Blog')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('server-api-docs', app, document)
+
+  await app.listen(4001)
+  console.log(`http://localhost:4001/server-api-docs`)
 }
 bootstrap()
