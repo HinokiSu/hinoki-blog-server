ede0b1c042f3c7bce2e5f18a94f3c1a0bc0820c2
Hinoki <yukihinoki@qq.com>
2022-02-28 21:49:18 +0800

    feat(server): add server app router

---
+++ apps/server/src/route/index.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@nestjs/core'
+import { ArticlesModule } from '../articles/articles.module'
+import { CategoriesModule } from '../categories/categories.module'
+
+// server应用的主路由
+export const serverRoute: Routes = [
+  {
+    path: 'server',
+    children: [
+      {
+        path: 'article',
+        module: ArticlesModule,
+      },
+      {
+        path: 'category',
+        module: CategoriesModule,
+      },
+    ],
+  },
+]
