c8a095e249bb4a017c7691770c25e6609c727f3d
Hinoki <yukihinoki@qq.com>
2022-02-28 21:50:19 +0800

    refactor(server): refacting server module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,8 +1,18 @@
+import { mongooseModule } from '@libs/db/hooks/mongoose.module'
+import { AppConfigurationModule } from '@libs/db/infrastructure/configuration/app-configuration.module'
 import { Module } from '@nestjs/common'
+import { RouterModule } from '@nestjs/core'
 import { ArticlesModule } from './articles/articles.module'
 import { CategoriesModule } from './categories/categories.module'
+import { serverRoute } from './route'
 
 @Module({
-  imports: [ArticlesModule, CategoriesModule],
+  imports: [
+    ArticlesModule,
+    CategoriesModule,
+    AppConfigurationModule,
+    mongooseModule,
+    RouterModule.register(serverRoute),
+  ],
 })
 export class AppModule {}
