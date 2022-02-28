7cbc7bc155126aad3ef8d1e20dec714bc5adde34
Hinoki <yukihinoki@qq.com>
2022-02-28 22:53:04 +0800

    feat(admin): add route & use new db module

--- apps/admin/src/admin.module.ts
+++ apps/admin/src/admin.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common'
+import { mongooseModule } from '@libs/db/hooks/mongoose.module'
+import { RouterModule } from '@nestjs/core'
 import { ArticleModule } from './article/article.module'
 import { CategoryModule } from './category/category.module'
+import { adminRoute } from './route'
+import { AppConfigurationModule } from '@libs/db/infrastructure/configuration/app-configuration.module'
 
 @Module({
-  imports: [ArticleModule, CategoryModule],
+  imports: [
+    ArticleModule,
+    CategoryModule,
+    AppConfigurationModule,
+    mongooseModule,
+    RouterModule.register(adminRoute),
+  ],
 })
 export class AdminModule {}
