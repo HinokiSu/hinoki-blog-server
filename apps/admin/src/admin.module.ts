a89a52bd61b802210416025edf4ccafac7104792
Hinoki <yukihinoki@qq.com>
2022-03-01 16:40:31 +0800

    chore(admin): modify importing module

--- apps/admin/src/admin.module.ts
+++ apps/admin/src/admin.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common'
-import { mongooseModule } from '@libs/db/hooks/mongoose.module'
 import { RouterModule } from '@nestjs/core'
 import { ArticleModule } from './article/article.module'
 import { CategoryModule } from './category/category.module'
 import { adminRoute } from './route'
-import { AppConfigurationModule } from '@libs/db/infrastructure/configuration/app-configuration.module'
+import { DatabaseModule } from '@libs/db'
 
 @Module({
-  imports: [
-    ArticleModule,
-    CategoryModule,
-    AppConfigurationModule,
-    mongooseModule,
-    RouterModule.register(adminRoute),
-  ],
+  imports: [DatabaseModule, ArticleModule, CategoryModule, RouterModule.register(adminRoute)],
 })
 export class AdminModule {}
