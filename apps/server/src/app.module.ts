6b5b96cde138c1a6b0f201a4a3cc562560625525
Hinoki <yukihinoki@qq.com>
2022-03-01 16:40:57 +0800

    chore(server): modify module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,5 +1,4 @@
-import { mongooseModule } from '@libs/db/hooks/mongoose.module'
-import { AppConfigurationModule } from '@libs/db/infrastructure/configuration/app-configuration.module'
+import { DatabaseModule } from '@libs/db'
 import { Module } from '@nestjs/common'
 import { RouterModule } from '@nestjs/core'
 import { ArticlesModule } from './articles/articles.module'
@@ -7,12 +6,6 @@ import { CategoriesModule } from './categories/categories.module'
 import { serverRoute } from './route'
 
 @Module({
-  imports: [
-    ArticlesModule,
-    CategoriesModule,
-    AppConfigurationModule,
-    mongooseModule,
-    RouterModule.register(serverRoute),
-  ],
+  imports: [ArticlesModule, CategoriesModule, DatabaseModule, RouterModule.register(serverRoute)],
 })
 export class AppModule {}
