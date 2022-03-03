004a48de83d9874f2f05f4733d539ffc9d9413b0
Hinoki <yukihinoki@qq.com>
2022-03-03 23:38:06 +0800

    chore(utils): update utils module

--- libs/utils/src/utils.module.ts
+++ libs/utils/src/utils.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { UtilsService } from './utils.service'
-
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [UtilsModule],
+      inject: [UtilsService],
+      useFactory: async () => ({
+        secret: UtilsService.getDataBaseConfig(),
+      }),
+    }),
+  ],
   providers: [UtilsService],
   exports: [UtilsService],
 })
--- libs/utils/src/utils.service.ts
+++ libs/utils/src/utils.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
-export class UtilsService {}
+export class UtilsService {
+  static getDataBaseConfig(): string {
+    throw new Error('Method not implemented.')
+  }
+  constructor(private config: ConfigService) {}
+
+  getJwtConfig(): string {
+    const jwtConfigEnv = this.config.get('JWT_SECRET')
+
+    if (!jwtConfigEnv) {
+      throw Error(`env file not have [JWT_SECRET] env param`)
+    }
+
+    return jwtConfigEnv
+  }
+}
