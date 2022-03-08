2f0b9d760dcae7b9c3bd552e1f5ccb8dff13e5f5
Hinoki <yukihinoki@qq.com>
2022-03-08 22:58:59 +0800

    chore: remove redundant files

--- libs/db/src/interfaces/category.interface.ts
+++
@@ -1,9 +0,0 @@
-// category interface
-
-import { Document } from 'mongoose'
-
-export interface CategoryDocument extends Document {
-  readonly name: string
-  readonly createAt: string
-  readonly updateAt: string
-}
